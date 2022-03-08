37273102cdec5cfcfe5c5799f3f53089567242cd
meraj@43 <mehrajmalik43@gmail.com>
2022-03-08 17:01:00 +0530

    send_email_verification_otp

--- src/components/Forms/SignUp.js
+++ src/components/Forms/SignUp.js
@@ -18,6 +18,9 @@ import {
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import { Close } from '@material-ui/icons';
 import userContext from '../../contexts/userCred/userContext';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 const useStyles = makeStyles((theme) => ({
   avatarStyle: {
@@ -26,27 +29,27 @@ const useStyles = makeStyles((theme) => ({
   btnFlex: {
     display: 'flex',
     justifyContent: 'space-between',
-    marginTop:'2px'
+    marginTop: '2px',
   },
   mediaAvatarStyle: {
     backgroundColor: '#1bbd7e',
     width: theme.spacing(3),
-    height: theme.spacing(3)
+    height: theme.spacing(3),
   },
   closeBtn: {
     position: 'absolute',
     right: '14px',
-    top: '20px'
+    top: '20px',
   },
   mediaCloseBtn: {
     position: 'absolute',
     right: '14px',
-    top: '13px'
+    top: '13px',
   },
-  my2:{
+  my2: {
     marginTop: '7px',
-  }
-}))
+  },
+}));
 
 const SignUp = (props) => {
   const userCon = useContext(userContext);
@@ -56,6 +59,10 @@ const SignUp = (props) => {
   const matches = useMediaQuery('(min-width:600px)');
 
   const [selected, setSelected] = useState('male');
+  const [otpModal, setOtpModal] = useState(false);
+
+  const [otp, setOtp] = useState('');
+  const [checkOTP, setCheckOTP] = useState('kkkk');
 
   const handleChange = (e) => {
     setSelected(e.target.value);
@@ -73,8 +80,31 @@ const SignUp = (props) => {
     setSignUpCreds({ ...signUpCreds, [e.target.name]: e.target.value });
   };
 
-  const onSubmit = async (e) => {
+  const onSignUp = async (e) => {
     e.preventDefault();
+    const respo = await fetch('http://localhost:8500/api/auth/getEmail', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(signUpCreds),
+    });
+
+    const respJson = await respo.json();
+    setCheckOTP(respJson.OTP);
+    setOtpModal(true);
+  };
+
+  const CheckOTP = (e) => {
+    if (checkOTP === otp) {
+      console.log(otp);
+      onSubmit();
+    }
+    console.log(otp, checkOTP);
+    setOtpModal(false);
+  };
+
+  const onSubmit = async () => {
     console.log(signUpCreds);
     console.log(selected);
     // -------------------------------------
@@ -91,7 +121,7 @@ const SignUp = (props) => {
       localStorage.setItem('renToken', Pjson.authToken);
       getUser();
     } else {
-      console.log("Error");
+      console.log('Error');
     }
     props.setIsSignUpOpen(false);
   };
@@ -110,26 +140,68 @@ const SignUp = (props) => {
   return (
     <Grid>
       <Grid align='center'>
+        <Modal
+          isOpen={otpModal}
+          style={{
+            overlay: {
+              backgroundColor: 'rgba(115,115,115,0.2)',
+            },
+            content: {
+              padding: 0,
+              width: '230px',
+              height: '205px',
+              top: '50%',
+              left: '30%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-around',
+            },
+          }}
+          // onRequestClose={() => setOtpModal(false)}
+        >
+          <Button onClick={() => setOtpModal(false)}>Close</Button>
+          <TextField
+            label='OTP'
+            variant='outlined'
+            name='OTP'
+            value={otp}
+            onChange={(e) => {
+              setOtp(e.target.value);
+            }}
+          />
+          <Button
+            size='small'
+            variant='contained'
+            color='primary'
+            onClick={CheckOTP}
+          >
+            Confirm
+          </Button>
+        </Modal>
         <Button
-          className={clsx(classes.closeBtn,{
-            [classes.mediaCloseBtn]: !matches
+          className={clsx(classes.closeBtn, {
+            [classes.mediaCloseBtn]: !matches,
           })}
           color='secondary'
           onClick={() => props.setIsSignUpOpen(false)}
         >
           <Close />
         </Button>
-        <Avatar className={clsx(classes.avatarStyle, {
-          [classes.mediaAvatarStyle]: !matches
-        })}>
+        <Avatar
+          className={clsx(classes.avatarStyle, {
+            [classes.mediaAvatarStyle]: !matches,
+          })}
+        >
           <AddCircleOutlineOutlinedIcon />
         </Avatar>
-        <h2 style={{ margin: '0', fontSize: !matches?'1.25rem':'1.5rem' }}>Sign Up</h2>
+        <h2 style={{ margin: '0', fontSize: !matches ? '1.25rem' : '1.5rem' }}>
+          Sign Up
+        </h2>
         <Typography variant='caption' gutterBottom>
           Please fill this form to create an account
         </Typography>
       </Grid>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSignUp}>
         <TextField
           label='Name'
           fullWidth
@@ -149,7 +221,9 @@ const SignUp = (props) => {
           required
         />
         <FormControl component='fieldset' className={classes.btnFlex}>
-          <FormLabel component='legend' className={classes.my2}>Gender</FormLabel>
+          <FormLabel component='legend' className={classes.my2}>
+            Gender
+          </FormLabel>
           <RadioGroup
             aria-label='gender'
             name='gender1'
@@ -198,14 +272,19 @@ const SignUp = (props) => {
         <Grid className={classes.btnFlex}>
           <Button
             onClick={handleReset}
-            size="small"
+            size='small'
             type='reset'
             variant='contained'
             color='secondary'
           >
             Reset
           </Button>
-          <Button type='submit' size="small" variant='contained' color='primary'>
+          <Button
+            type='submit'
+            size='small'
+            variant='contained'
+            color='primary'
+          >
             Sign Up
           </Button>
         </Grid>
